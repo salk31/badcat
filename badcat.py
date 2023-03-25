41d73f270be6f5bfc8bdf0a69b18bb6e9c8eefe9
Sam Hough <samuel.hough@diamond.ac.uk>
2023-03-25 18:49:48 +0000

    imports

---
+++ badcat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
+
+interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+
+classify_lite = interpreter.get_signature_runner('serving_default')
+
+predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
+score_lite = tf.nn.softmax(predictions_lite)
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
+)
