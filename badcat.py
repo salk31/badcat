1008946b3acf58e3e8d1ece05d7158f22a0cb155
Sam Hough <samuel.hough@diamond.ac.uk>
2023-03-26 17:17:25 +0100

    pi@raspberrypi:~/badcat $ python3 badcat.py starting got interpreter got classify Traceback (most recent call last):   File "/home/pi/badcat/badcat.py", line 13, in <module>     predictions_lite = classify_lite(sequential_1_input=img_array)['outputs'] NameError: name 'img_array' is not defined

--- badcat.py
+++ badcat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import numpy as np
 from tflite_runtime.interpreter import Interpreter
 
 TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
@@ -9,6 +10,12 @@ print("got interpreter")
 
 classify_lite = interpreter.get_signature_runner('serving_default')
 print("got classify")
+
+
+sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+img = Image.open( image_url ).convert('RGB').resize((224, 224))
+img_array = np.array ( img, dtype=np.float32 )
+
 
 predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
 score_lite = tf.nn.softmax(predictions_lite)
