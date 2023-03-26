4f652f57fde0486f760ef76dd0f3de76113b2be5
Sam Hough <samuel.hough@diamond.ac.uk>
2023-03-26 17:39:43 +0100

    hmmmm

--- badcat.py
+++ badcat.py
@@ -1,28 +1,114 @@
-#!/usr/bin/env python3
+"""label_image for tflite."""
 
-from PIL import Image
+import argparse
+import time
+
 import numpy as np
-from tflite_runtime.interpreter import Interpreter
-
-TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
-print("starting")
-interpreter = Interpreter(model_path=TF_MODEL_FILE_PATH)
-print("got interpreter")
-
-classify_lite = interpreter.get_signature_runner('serving_default')
-print("got classify")
+from PIL import Image
+import tflite_runtime.interpreter as tflite
 
 
-image_url = "data/clara/Camera1_06-21-22.jpg"
-img = Image.open( image_url ).convert('RGB').resize((180, 180))
-img_array = np.array ( img, dtype=np.float32 )
-img_array = np.expand_dims(img_array, axis=0)/255
+def load_labels(filename):
+  with open(filename, 'r') as f:
+    return [line.strip() for line in f.readlines()]
 
 
-predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
-score_lite = tf.nn.softmax(predictions_lite)
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '-i',
+      '--image',
+      default='data/clara/Camera1_06_21-22.jpg',
+      help='image to be classified')
+  parser.add_argument(
+      '-m',
+      '--model_file',
+      default='model.tflite.tflite',
+      help='.tflite model to be executed')
+  parser.add_argument(
+      '-l',
+      '--label_file',
+      default='/tmp/labels.txt',
+      help='name of file containing labels')
+  parser.add_argument(
+      '--input_mean',
+      default=127.5, type=float,
+      help='input_mean')
+  parser.add_argument(
+      '--input_std',
+      default=127.5, type=float,
+      help='input standard deviation')
+  parser.add_argument(
+      '--num_threads', default=None, type=int, help='number of threads')
+  parser.add_argument(
+      '-e', '--ext_delegate', help='external_delegate_library path')
+  parser.add_argument(
+      '-o',
+      '--ext_delegate_options',
+      help='external delegate options, \
+            format: "option1: value1; option2: value2"')
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
-)
+  args = parser.parse_args()
+
+  ext_delegate = None
+  ext_delegate_options = {}
+
+  # parse extenal delegate options
+  if args.ext_delegate_options is not None:
+    options = args.ext_delegate_options.split(';')
+    for o in options:
+      kv = o.split(':')
+      if (len(kv) == 2):
+        ext_delegate_options[kv[0].strip()] = kv[1].strip()
+      else:
+        raise RuntimeError('Error parsing delegate option: ' + o)
+
+  # load external delegate
+  if args.ext_delegate is not None:
+    print('Loading external delegate from {} with args: {}'.format(
+        args.ext_delegate, ext_delegate_options))
+    ext_delegate = [
+        tflite.load_delegate(args.ext_delegate, ext_delegate_options)
+    ]
+
+  interpreter = tflite.Interpreter(
+      model_path=args.model_file,
+      experimental_delegates=ext_delegate,
+      num_threads=args.num_threads)
+  interpreter.allocate_tensors()
+
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+
+  # check the type of the input tensor
+  floating_model = input_details[0]['dtype'] == np.float32
+
+  # NxHxWxC, H:1, W:2
+  height = input_details[0]['shape'][1]
+  width = input_details[0]['shape'][2]
+  img = Image.open(args.image).resize((width, height))
+
+  # add N dim
+  input_data = np.expand_dims(img, axis=0)
+
+  if floating_model:
+    input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+
+  interpreter.set_tensor(input_details[0]['index'], input_data)
+
+  start_time = time.time()
+  interpreter.invoke()
+  stop_time = time.time()
+
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  results = np.squeeze(output_data)
+
+  top_k = results.argsort()[-5:][::-1]
+  labels = load_labels(args.label_file)
+  for i in top_k:
+    if floating_model:
+      print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
+    else:
+      print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+
+  print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
