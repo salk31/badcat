3a68454f274e2e481d20fa4039e6d2f81c386292
Sam Hough <samuel.hough@diamond.ac.uk>
2023-03-27 14:31:56 +0100

    no chance

--- core.py
+++ core.py
@@ -1,1 +1,76 @@
+import numpy as np
+from PIL import Image
+import tflite_runtime.interpreter as tflite
+
+import time
+
+
 print("hello from core")
+
+
+def init():
+  ext_delegate = None
+  ext_delegate_options = {}
+
+  # parse extenal delegate options
+  if args.ext_delegate_options is not None:
+    options = args.ext_delegate_options.split(';')
+    for o in options:
+      kv = o.split(':')
+      if (len(kv) == 2):
+        ext_delegate_options[kv[0].strip()] = kv[1].strip()
+      else:
+        raise RuntimeError('Error parsing delegate option: ' + o)
+
+  # load external delegate
+  if args.ext_delegate is not None:
+    print('Loading external delegate from {} with args: {}'.format(
+        args.ext_delegate, ext_delegate_options))
+    ext_delegate = [
+        tflite.load_delegate(args.ext_delegate, ext_delegate_options)
+    ]
+
+  interpreter = tflite.Interpreter(
+      model_path=args.model_file,
+      experimental_delegates=ext_delegate,
+      num_threads=args.num_threads)
+  interpreter.allocate_tensors()
+
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+
+  # check the type of the input tensor
+  floating_model = input_details[0]['dtype'] == np.float32
+
+  # NxHxWxC, H:1, W:2
+  height = input_details[0]['shape'][1]
+  width = input_details[0]['shape'][2]
+  
+  
+def process(image):
+  img = Image.open(image).resize((width, height))
+
+  # add N dim
+  input_data = np.expand_dims(img, axis=0)
+
+  if floating_model:
+    input_data = np.float32(input_data)
+
+  interpreter.set_tensor(input_details[0]['index'], input_data)
+
+  start_time = time.time()
+  interpreter.invoke()
+  stop_time = time.time()
+
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  results = np.squeeze(output_data)
+
+  top_k = results.argsort()[-5:][::-1]
+  labels = ['badb', 'badbw', 'clara']
+  for i in top_k:
+    if floating_model:
+      print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
+    else:
+      print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+
+  print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
