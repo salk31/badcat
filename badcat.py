66282e434ce484671e59907774a734fa8a6c1251
Sam Hough <samuel.hough@diamond.ac.uk>
2023-03-27 14:42:30 +0100

    yuk

--- badcat.py
+++ badcat.py
@@ -10,5 +10,5 @@ import core
 
 if __name__ == '__main__':
 
-  core.init()
+  core = new core.Core()
   core.process('data/clara/Camera1_06-21-22.jpg')
--- core.py
+++ core.py
@@ -7,55 +7,55 @@ import time
 
 print("hello from core")
 
+class Core:
+  def __init__(self):
+    model_file = 'model.tflite'
+    num_threads = 1
 
-def init():
-  model_file = 'model.tflite'
-  num_threads = 1
+    ext_delegate = None
+    ext_delegate_options = {}
 
-  ext_delegate = None
-  ext_delegate_options = {}
+    self.interpreter = tflite.Interpreter(
+        model_path=model_file,
+        experimental_delegates=ext_delegate,
+        num_threads=num_threads)
+    self.interpreter.allocate_tensors()
 
-  interpreter = tflite.Interpreter(
-      model_path=model_file,
-      experimental_delegates=ext_delegate,
-      num_threads=num_threads)
-  interpreter.allocate_tensors()
+    self.input_details = self.interpreter.get_input_details()
+    self.output_details = self.interpreter.get_output_details()
 
-  input_details = interpreter.get_input_details()
-  output_details = interpreter.get_output_details()
+    # check the type of the input tensor
+    self.floating_model = self.input_details[0]['dtype'] == np.float32
 
-  # check the type of the input tensor
-  floating_model = input_details[0]['dtype'] == np.float32
-
-  # NxHxWxC, H:1, W:2
-  height = input_details[0]['shape'][1]
-  width = input_details[0]['shape'][2]
+    # NxHxWxC, H:1, W:2
+    self.height = self.input_details[0]['shape'][1]
+    self.width = self.input_details[0]['shape'][2]
   
   
-def process(image):
-  img = Image.open(image).resize((width, height))
+  def process(image):
+    img = Image.open(image).resize((self.width, self.height))
 
-  # add N dim
-  input_data = np.expand_dims(img, axis=0)
+    # add N dim
+    input_data = np.expand_dims(img, axis=0)
 
-  if floating_model:
-    input_data = np.float32(input_data)
-
-  interpreter.set_tensor(input_details[0]['index'], input_data)
-
-  start_time = time.time()
-  interpreter.invoke()
-  stop_time = time.time()
-
-  output_data = interpreter.get_tensor(output_details[0]['index'])
-  results = np.squeeze(output_data)
-
-  top_k = results.argsort()[-5:][::-1]
-  labels = ['badb', 'badbw', 'clara']
-  for i in top_k:
     if floating_model:
-      print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
-    else:
-      print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+      input_data = np.float32(input_data)
 
-  print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
+    self.interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    start_time = time.time()
+    self.interpreter.invoke()
+    stop_time = time.time()
+
+    output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+    results = np.squeeze(output_data)
+
+    top_k = results.argsort()[-5:][::-1]
+    labels = ['badb', 'badbw', 'clara']
+    for i in top_k:
+      if floating_model:
+        print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
+      else:
+        print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+
+    print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
