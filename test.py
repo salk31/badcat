408cc7236947f8751e7b779c26ecd2a68482f73e
Sam Hough <samuel.hough@diamond.ac.uk>
2023-03-27 14:58:31 +0100

    never going to work

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 #!/usr/local/bin/python3.9
 
 import core
+import pathlib
+
+core = core.Core()
+
+data_dir = pathlib.Path("test/data")
+
+for path : data_dir.glob("*/*.jpg"):
+  core.process(path)
