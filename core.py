46e6ac1efad988242243a959cbfd4e81bafb4107
Sam Hough <samuel.hough@diamond.ac.uk>
2023-04-21 14:03:39 +0100

    never going to work

--- core.py
+++ core.py
@@ -9,56 +9,49 @@ print("hello from core")
 
 class Core:
   def __init__(self):
-    model_file = 'model.tflite'
-    num_threads = 1
+    model_file = 'custom_model_lite/detect.tflite'
+    lblpath = 'custom_model_lite/labelmap.txt'
 
-    ext_delegate = None
-    ext_delegate_options = {}
+    with open(lblpath, 'r') as f:
+      labels = [line.strip() for line in f.readlines()]
 
-    self.interpreter = tflite.Interpreter(model_path=model_file)
-    #self.interpreter.allocate_tensors()
+    # Load the Tensorflow Lite model into memory
+    self.interpreter = Interpreter(model_path=modelpath)
+    self.interpreter.allocate_tensors()
 
-    self.input_details = self.interpreter.get_input_details()
-    self.output_details = self.interpreter.get_output_details()
+    # Get model details
+    input_details = self.interpreter.get_input_details()
+    output_details = self.interpreter.get_output_details()
+    self.height = input_details[0]['shape'][1]
+    self.width = input_details[0]['shape'][2]
 
-    # check the type of the input tensor
-    self.floating_model = self.input_details[0]['dtype'] == np.float32
+    self.float_input = (input_details[0]['dtype'] == np.float32)
 
-    # NxHxWxC, H:1, W:2
-    self.height = self.input_details[0]['shape'][1]
-    self.width = self.input_details[0]['shape'][2]
+    input_mean = 127.5
+    input_std = 127.5
   
   
   def process(self, image):
-    img = Image.open(image).resize((self.width, self.height))
+    # Load image and resize to expected shape [1xHxWx3]
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    imH, imW, _ = image.shape 
+    image_resized = cv2.resize(image_rgb, (self.width, self.height))
+    input_data = np.expand_dims(image_resized, axis=0)
 
-    # add N dim
-    img_array = np.expand_dims(img, axis=0)
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    if float_input:
+      input_data = (np.float32(input_data) - input_mean) / input_std
 
-    if self.floating_model:
-      img_array = np.float32(img_array)
+    # Perform the actual detection by running the model with the image as input
+    self.interpreter.set_tensor(input_details[0]['index'],input_data)
+    self.interpreter.invoke()
+
+    # Retrieve detection results
+    boxes = self.interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
+    classes = self.interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
+    scores = self.interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
     
-    #print(str(img_array))
-  
-
-    #self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
-
-    start_time = time.time()
-    #self.interpreter.invoke()
-    classify_lite = self.interpreter.get_signature_runner('serving_default')
-    predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
-    stop_time = time.time()
-
-    #output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-    #results = np.squeeze(output_data)
-    print(str(predictions_lite))
-
-    #top_k = results.argsort()[-5:][::-1]
-    #labels = ['badbw', 'clara']
-    #for i in top_k:
-    #  if self.floating_model:
-    #    print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
-    #  else:
-    #    print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
-
-    print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
+    print(str(classes))
+    print(str(scores))
+    
