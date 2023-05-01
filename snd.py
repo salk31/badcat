996c199a4ef0236dcbf309c51093c7479dfa870f
Sam Hough <samuel.hough@diamond.ac.uk>
2023-05-01 17:09:30 +0100

    fancy

--- snd.py
+++ snd.py
@@ -1,10 +1,17 @@
 import pygame
 
-
+class Snd:
+  def __init__(self):
+    pygame.mixer.init()
+    pygame.mixer.music.load("snd/mg42a.wav")
+  
+  def play(self):
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+      continue
+      
 if __name__ == '__main__':
-  pygame.mixer.init()
-  pygame.mixer.music.load("snd/mg42a.wav")
-  pygame.mixer.music.play()
-  while pygame.mixer.music.get_busy() == True:
-    continue
+  snd = Snd()
+  snd.play()
+
     
