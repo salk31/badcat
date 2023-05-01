6b371b1306b04a0901f854f1aeaf81899427ea93
Sam Hough <samuel.hough@diamond.ac.uk>
2023-05-01 16:43:11 +0100

    doh

--- snd.py
+++ snd.py
@@ -3,7 +3,7 @@ import pygame
 
 if __name__ == '__main__':
   pygame.mixer.init()
-  pygame.mixer.music.load("m34short.wav")
+  pygame.mixer.music.load("snd/mg42a.wav")
   pygame.mixer.music.play()
   while pygame.mixer.music.get_busy() == True:
     continue
