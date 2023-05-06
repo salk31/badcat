675841c59cfb9a36e3ba48db70044ce7917fdf70
Sam Hough <samuel.hough@diamond.ac.uk>
2023-05-06 16:09:04 +0100

    sigh

--- snd.py
+++ snd.py
@@ -1,14 +1,21 @@
-import pygame
+import subprocess
+
+# for simple commands
+subprocess.run(["ls", "-l"]) 
+
+#import pygame
 
 class Snd:
   def __init__(self):
-    pygame.mixer.init()
-    pygame.mixer.music.load("snd/mg42a.wav")
+    #pygame.mixer.init()
+    #pygame.mixer.music.load("snd/mg42a.wav")
+    print("snd")
   
   def play(self):
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-      continue
+    #pygame.mixer.music.play()
+    #while pygame.mixer.music.get_busy() == True:
+    #  continue
+    subprocess.run(["aplay", "snd/mg42a.wav"]) 
       
 if __name__ == '__main__':
   snd = Snd()
