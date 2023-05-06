09f42d7a9413935c7acb1bf646cdd3c0d302cec9
Sam Hough <samuel.hough@diamond.ac.uk>
2023-05-06 15:44:52 +0100

    UnboundLocalError: local variable 'fire' referenced before assignment

--- badcat.py
+++ badcat.py
@@ -11,36 +11,40 @@ from watchdog.events import FileSystemEventHandler
 
 
 class MyHandler(FileSystemEventHandler):
-  def on_any_event(self, event):
-    print(event.event_type, event.src_path)
+
+  def __init__(self):
+    self.core = core.Core()
+    self.snd = snd.Snd()
+    self.fire = 0
+
+  #def on_any_event(self, event):
+  #  print(event.event_type, event.src_path)
 
   def on_created(self, event):
     print("on_created", event.src_path)
     print(event.src_path.strip())
-    
-    res = core.process(event.src_path)
+    image = event.src_path
+    res = self.core.process(image)
     detections = res.detections(0.8)
     
     if (len(detections) == 0):
-      fire = 0
+      self.fire = 0
     else:
       got = detections[0]
       if (got[0] == 'bad'):
-        fire += 1
+        self.fire += 1
       else:
-        fire = 0
+        self.fire = 0
         
-    print(f'{image} -> {detections} -> {fire}')
-    if (fire > 30):
-      fire = 0
-      snd.play()
+    print(f'{image} -> {detections} -> {self.fire}')
+    if (self.fire > 30):
+      self.fire = 0
+      self.snd.play()
 
 
 if __name__ == '__main__':
 
-  core = core.Core()
-  snd = snd.Snd()
-  fire = 0
+
   
   root = '/var/lib/motioneye/' # Camera1/2023-04-20/'
   
